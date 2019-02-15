6269bec47de7a293a22cd48d48454277dea9eced
Marsswiss <lars.luginbuehl@gmail.com>
2019-02-15 10:14:12 +0100

    Update Temp_Arduino.ino

--- Temp_Arduino.ino
+++ Temp_Arduino.ino
@@ -1,19 +1,19 @@
-
-void setup(){
+void setup() {
+  //Init Communication
   Serial.begin(9600);
 }
 
-
 void loop(){
-  float voltage, degreesC;
+  //Read Voltage
   float reading = (float)analogRead(0);
-  voltage = reading *5/1024;
-  degreesC = (voltage - 0.5) * 100.0;
-  Serial.print("reading: ");
-  Serial.print(reading);
-  Serial.print(" voltage: ");
-  Serial.print(voltage);
-  Serial.print("  deg C: ");
-  Serial.println(degreesC);   
-  delay(1000);
+  float voltage = reading *5/1024;
+  //Compute Temperature
+  float degreesC = (voltage - 0.5) * 100.0;
+  //Filter Errors
+  if(degreesC > 10){
+    //Send Temperature
+    Serial.println(degreesC);
+    //Wait a second
+    delay(1000); 
+  }
 }
